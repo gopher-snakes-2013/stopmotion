c40e82febb2f52e8f6e1ee5b4b430651ecd7b437
Apprentice <apprentice@dbc13.local>
2013-10-22 15:41:06 -0700

    user can get real time arrival information for one stop

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source "https://rubygems.org"
 
 gem 'sinatra'
+gem 'nokogiri'
+gem 'open-uri'
+
 gem 'shotgun'
 
 gem 'rspec'
--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require 'nokogiri'
+require 'open-uri'
 
 get '/' do
   erb :index
+end
+
+get '/predict' do 
+  @next_arrival = predict_n_montogomery
+  erb :predict
+end
+
+
+
+
+#HELPERS
+
+def predict_n_montogomery
+  predictions = Nokogiri::XML(open('http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&r=N&s=6994'))
+  prediction_nodes = predictions.xpath('.//prediction')
+  prediction_minutes = prediction_nodes.map {|prediction| prediction[:minutes]}
+  prediction_minutes[0]
 end
