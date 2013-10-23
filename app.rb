41afbe2264a9ec808cdeeaebc226eadf884a52bd
Apprentice <apprentice@dbc13.local>
2013-10-22 17:18:36 -0700

    User can select a stop to view departure times

--- app.rb
+++ app.rb
@@ -7,18 +7,32 @@ get '/' do
 end
 
 get '/predict' do 
-  @next_arrival = predict_n_montogomery
+  @stop = params[:stop]
+  @next_arrival = minutes_until_next_arrival("N",@stop)
+  @stop = get_stop_title(@stop)
   erb :predict
 end
 
+post '/predict' do 
+end  
 
 
 
 #HELPERS
 
-def predict_n_montogomery
-  predictions = Nokogiri::XML(open('http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&r=N&s=6994'))
+def minutes_until_next_arrival(line, stop)
+  predictions = Nokogiri::XML(open("http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&r=N&s=#{stop}"))
   prediction_nodes = predictions.xpath('.//prediction')
   prediction_minutes = prediction_nodes.map {|prediction| prediction[:minutes]}
   prediction_minutes[0]
+end
+
+def get_stop_for_line(line)
+
+end
+
+
+def get_stop_title(stop_tag)
+  @stop_tags_and_titles = {"6994" => "Montgomery" ,"6995" => "Powell" ,"7217" => "Embarcadero"}
+  @stop_tags_and_titles[stop_tag]
 end
